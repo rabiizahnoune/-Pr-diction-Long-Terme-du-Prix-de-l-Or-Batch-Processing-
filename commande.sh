4c0228332737f28998867f1b17751cc412a3d112
rabiizahnoune <rabiizahnoue05@gmail.com>
2025-02-15 20:50:57 +0100

    second_commit

--- commande.sh
+++ commande.sh
@@ -1,3 +1,5 @@
 
 #Récupérer l'adresse IP du conteneur PostgreSQL
 docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgres_airflow
+#entrer dans le contenaire de postgres
+docker exec -it postgres_airflow bash
